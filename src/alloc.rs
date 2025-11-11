c234cebcd3b53197edb78ddef306aefbfe148c7d
Sakii <13037299+SakiiCode@users.noreply.github.com>
2025-11-11 19:12:38 +0100

    rusty alloc implementation

--- src/alloc.rs
+++ src/alloc.rs
@@ -1,14 +1,11 @@
 //! Set LVGL allocator as Rust's global memory allocator
 
 use core::alloc::{GlobalAlloc, Layout};
+use core::ffi::c_void;
 
 // Register the global allocator
 #[global_allocator]
 static ALLOCATOR: LvglAlloc = LvglAlloc;
-
-const fn align_up(num: usize, align: usize) -> usize {
-    ((num) + ((align) - 1)) & !((align) - 1)
-}
 
 /// LVGL allocator. Enabled by toggling the `lvgl-alloc` feature.
 pub struct LvglAlloc;
@@ -16,20 +13,22 @@ pub struct LvglAlloc;
 unsafe impl GlobalAlloc for LvglAlloc {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         unsafe {
-            const PTR_OFFSET_SZ: usize = (usize::BITS / u8::BITS) as usize;
-            let hdr_size = PTR_OFFSET_SZ + (layout.align() - 1);
-            let raw = lightvgl_sys::lv_malloc(layout.size() + hdr_size) as *mut u8;
-            let aligned = align_up(raw.add(PTR_OFFSET_SZ) as usize, layout.align()) as *mut u8;
-            *(aligned.clone().sub(PTR_OFFSET_SZ) as *mut usize) = aligned.offset_from(raw) as usize;
+            const USIZE_BYTES: usize = (usize::BITS / u8::BITS) as usize;
+            let extra_bytes = USIZE_BYTES + (layout.align() - 1);
+            let raw = lightvgl_sys::lv_malloc(layout.size() + extra_bytes).cast::<u8>();
+            let raw_shifted = raw.add(USIZE_BYTES);
+            let offset = raw_shifted.align_offset(layout.align());
+            let aligned = raw_shifted.wrapping_add(offset);
+            *((aligned.cast::<usize>()).sub(1)) = offset + USIZE_BYTES;
             aligned
         }
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
         unsafe {
-            let offset = *(ptr as *mut usize).sub(1);
+            let offset = *(ptr.cast::<usize>()).sub(1);
             let raw = ptr.sub(offset);
-            lightvgl_sys::lv_free(raw as *mut core::ffi::c_void)
+            lightvgl_sys::lv_free(raw.cast::<c_void>())
         }
     }
 }
