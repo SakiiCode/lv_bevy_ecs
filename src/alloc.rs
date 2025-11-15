05cb76252653188ab41d025db1caf23f3b63fe5b
Sakii <13037299+SakiiCode@users.noreply.github.com>
2025-11-15 17:49:09 +0100

    implement alloc_zeroed and realloc

--- src/alloc.rs
+++ src/alloc.rs
@@ -28,7 +28,52 @@ unsafe impl GlobalAlloc for LvglAlloc {
         unsafe {
             let offset = *(ptr.cast::<usize>()).sub(1);
             let raw = ptr.sub(offset);
-            lightvgl_sys::lv_free(raw.cast::<c_void>())
+            lightvgl_sys::lv_free(raw.cast::<c_void>());
+        }
+    }
+
+    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
+        unsafe {
+            const USIZE_BYTES: usize = (usize::BITS / u8::BITS) as usize;
+            let extra_bytes = USIZE_BYTES + (layout.align() - 1);
+            let raw = lightvgl_sys::lv_calloc(layout.size() + extra_bytes, 1).cast::<u8>();
+            let raw_shifted = raw.add(USIZE_BYTES);
+            let offset = raw_shifted.align_offset(layout.align());
+            let aligned = raw_shifted.wrapping_add(offset);
+            *((aligned.cast::<usize>()).sub(1)) = offset + USIZE_BYTES;
+            aligned
+        }
+    }
+
+    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
+        unsafe {
+            /*const USIZE_BYTES: usize = (usize::BITS / u8::BITS) as usize;
+            let extra_bytes = USIZE_BYTES + (layout.align() - 1);
+            let raw = lightvgl_sys::lv_malloc(new_size + extra_bytes).cast::<u8>();
+            let raw_shifted = raw.add(USIZE_BYTES);
+            let offset = raw_shifted.align_offset(layout.align());
+            let aligned = raw_shifted.wrapping_add(offset);
+            *((aligned.cast::<usize>()).sub(1)) = offset + USIZE_BYTES;
+            lightvgl_sys::lv_memcpy(
+                aligned.cast::<c_void>(),
+                ptr.cast::<c_void>(),
+                layout.size(),
+            );
+            self.dealloc(ptr, layout);
+            aligned*/
+
+            let offset = *(ptr.cast::<usize>()).sub(1);
+            let old = ptr.sub(offset).cast::<c_void>();
+
+            const USIZE_BYTES: usize = (usize::BITS / u8::BITS) as usize;
+            let extra_bytes = USIZE_BYTES + (layout.align() - 1);
+
+            let raw = lightvgl_sys::lv_realloc(old, new_size + extra_bytes).cast::<u8>();
+            let raw_shifted = raw.add(USIZE_BYTES);
+            let offset = raw_shifted.align_offset(layout.align());
+            let aligned = raw_shifted.wrapping_add(offset);
+            *((aligned.cast::<usize>()).sub(1)) = offset + USIZE_BYTES;
+            aligned
         }
     }
 }
