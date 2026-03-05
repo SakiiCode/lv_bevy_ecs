ad011966fb8c275911fe8aaecc2bd499349d1d31
Sakii <13037299+SakiiCode@users.noreply.github.com>
2026-03-06 00:44:11 +0100

    implement alloc as LVGL heap allocator

--- src/malloc.rs
+++ src/malloc.rs
@@ -1,21 +1,90 @@
-use core::ffi::c_void;
+use core::{alloc::Layout, ffi::c_void};
+
+use lightvgl_sys::{lv_mem_monitor_t, lv_result_t, lv_result_t_LV_RESULT_OK};
+
+// void * lv_malloc_core(size_t size);
+// void * lv_realloc_core(void * p, size_t new_size);
+// void lv_free_core(void * p);
+// void lv_mem_monitor_core(lv_mem_monitor_t * mon_p);
+// lv_result_t lv_mem_test_core(void);
+
+#[cfg(not(LV_USE_STDLIB_MALLOC = "CUSTOM"))]
+compile_error!("`rust-alloc` feature requires `LV_USE_STDLIB_MALLOC = CUSTOM`");
+
+#[cfg(feature = "lvgl-alloc")]
+compile_error!("`rust-alloc` feature must not be used together with `lvgl-alloc`");
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn malloc(size: usize) -> *mut c_void {
-    unsafe { lightvgl_sys::lv_malloc(size) }
+pub unsafe extern "C" fn lv_mem_init() {}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn lv_malloc_core(size: usize) -> *mut c_void {
+    unsafe {
+        const USIZE_BYTES: usize = (usize::BITS / u8::BITS) as usize;
+
+        let new_size = size + USIZE_BYTES;
+        let layout = Layout::array::<u8>(new_size).unwrap();
+
+        let raw = alloc::alloc::alloc(layout);
+        let raw_shifted = raw.add(USIZE_BYTES);
+        *((raw_shifted.cast::<usize>()).sub(1)) = new_size;
+        raw_shifted.cast::<c_void>()
+    }
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn calloc(num: usize, size: usize) -> *mut c_void {
-    unsafe { lightvgl_sys::lv_calloc(num, size) }
+pub unsafe extern "C" fn lv_realloc_core(ptr: *mut c_void, new_size: usize) -> *mut c_void {
+    unsafe {
+        const USIZE_BYTES: usize = (usize::BITS / u8::BITS) as usize;
+        let ptr = ptr.cast::<u8>();
+
+        if ptr.is_null() {
+            return lv_malloc_core(new_size);
+        }
+
+        let old_size = *(ptr.cast::<usize>()).sub(1);
+        let old_raw = ptr.sub(USIZE_BYTES);
+        let old_layout = Layout::array::<u8>(old_size).unwrap();
+
+        let new_size = new_size + USIZE_BYTES;
+
+        let raw = alloc::alloc::realloc(old_raw, old_layout, new_size);
+        let raw_shifted = raw.add(USIZE_BYTES);
+        *((raw_shifted.cast::<usize>()).sub(1)) = new_size;
+        raw_shifted.cast::<c_void>()
+    }
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn realloc(data_p: *mut c_void, size: usize) -> *mut c_void {
-    unsafe { lightvgl_sys::lv_realloc(data_p, size) }
+pub unsafe extern "C" fn lv_free_core(ptr: *mut c_void) {
+    unsafe {
+        const USIZE_BYTES: usize = (usize::BITS / u8::BITS) as usize;
+        let ptr = ptr.cast::<u8>();
+
+        let old_size = *(ptr.cast::<usize>()).sub(1);
+        let old_raw = ptr.sub(USIZE_BYTES);
+        let old_layout = Layout::array::<u8>(old_size).unwrap();
+
+        alloc::alloc::dealloc(old_raw, old_layout);
+    }
+}
+
+pub trait MemoryStats {
+    fn get_memory_stats(monitor: &mut lv_mem_monitor_t);
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn free(data: *mut c_void) {
-    unsafe { lightvgl_sys::lv_free(data) }
+pub unsafe extern "C" fn lv_mem_monitor_core(monitor: *mut lv_mem_monitor_t) {
+    unsafe extern "Rust" {
+        fn get_memory_stats(monitor: &mut lv_mem_monitor_t);
+    }
+
+    unsafe {
+        get_memory_stats(monitor.as_mut().unwrap());
+    }
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn lv_mem_test_core() -> lv_result_t {
+    lv_result_t_LV_RESULT_OK
 }
